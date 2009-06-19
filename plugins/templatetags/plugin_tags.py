2615d563f2ed7e6bf62eb92d3c8e589d0eaa84ef
Hugh Brown <hughdbrown@yahoo.com>
2009-06-19 18:07:53 -0400

    Simplify library plugin

--- plugins/templatetags/plugin_tags.py
+++ plugins/templatetags/plugin_tags.py
@@ -1,18 +1,21 @@
 from django import template
-from django.core.exceptions import ObjectDoesNotExist
+#from django.core.exceptions import ObjectDoesNotExist
 
 from belleville.plugins.books.models import Book
 register = template.Library()
 
+def _library_all():
+    return Book.objects.all().order_by('-created_at')
+
 def library_list():
-    current_books = Book.objects.filter(status=2).order_by('-created_at')[:1]
-    tbr_books = Book.objects.filter(status=3).order_by('-created_at')[:5]
-    past_books = Book.objects.filter(status=1).order_by('-created_at')[:5]
+    all_books = _library_all()
+    current_books = all_books.filter(status=2)[:1]
+    tbr_books = all_books.filter(status=3)[:5]
+    past_books = all_books.filter(status=1)[:5]
     return {'current_books': current_books, 'tbr_books': tbr_books, 'past_books': past_books}
 register.inclusion_tag('library_list.html')(library_list)
 
 def library_all():
-    all_books = Book.objects.all().order_by('-created_at')
-    return {'all_books': all_books}
+    return {'all_books': _library_all()}
 register.inclusion_tag('all_books.html')(library_all)
 
