abde8e7f37481bf33d53507cb710ba478beb30d6
Hugh Brown <hughdbrown@yahoo.com>
2009-06-19 18:13:17 -0400

    cut whitespace

--- plugins/books/admin.py
+++ plugins/books/admin.py
@@ -5,5 +5,3 @@ from belleville.plugins.books.models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author_last_name', 'author_first_name', 'status', 'rating',)
     list_filter = ('status', 'rating',)
-
-
